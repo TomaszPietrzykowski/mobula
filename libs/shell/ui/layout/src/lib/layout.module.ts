d4342bf96a1863d41cbc72b8f166bbb2f738a326
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2023-11-27 22:28:37 +0100

    layout components added

--- libs/shell/ui/layout/src/lib/layout.module.ts
+++ libs/shell/ui/layout/src/lib/layout.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { MainViewModule } from '@mobula/main-view';
 import { LayoutComponent } from './layout.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent, FooterModule } from '@mobula/footer';
+import { HeaderModule, HeaderComponent } from '@mobula/header';
 
 @NgModule({
-  imports: [CommonModule, MainViewModule, RouterModule],
-  declarations: [LayoutComponent],
+  imports: [
+    CommonModule,
+    MainViewModule,
+    RouterModule,
+    FooterModule,
+    HeaderModule,
+  ],
+  declarations: [LayoutComponent, FooterComponent, HeaderComponent],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
