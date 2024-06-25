ae408fd449a42a06ccd0c8f3af261daa261a6ade
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2024-06-25 21:30:24 +0200

    deployment pipeline refactored to promises AB#89

--- mydevil.pipeline.js
+++ mydevil.pipeline.js
@@ -13,87 +13,75 @@ console.log(
 conn.on('ready', () => {
     console.log('\x1b[38;5;87mConnection established.\n\x1b[0m');
     // 1. Remove old build
-    console.log('\x1b[38;5;87mRemoving old build.\n\x1b[0m');
     conn.exec(`find ${remotePath} -mindepth 1 -delete`, (err, stream) => {
         if (err) throw err;
+        console.log('\x1b[38;5;87mRemoving old build.\n\x1b[0m');
 
         stream
-            .on('close', (code, signal) => {})
+            .on('close', (code, signal) => {
+                // 2. Upload build:
+                conn.sftp((err, sftp) => {
+                    if (err) throw err;
+                    console.log(
+                        `\x1b[38;5;87mCopying files${
+                            env.VERBOSE_LOG ? ': ' : '...\n'
+                        }\x1b[0m`
+                    );
+
+                    uploadDirPromise(sftp, localDistPath, remotePath).then(
+                        () => {
+                            // 3. Run npm install in the remote build directory
+                            conn.exec(
+                                `cd ${remotePath} && npm install`,
+                                (err, stream) => {
+                                    if (err) throw err;
+                                    console.log(
+                                        `${
+                                            env.VERBOSE_LOG ? '\n' : ''
+                                        }\x1b[38;5;87mInstalling dependencies...\n\x1b[0m`
+                                    );
+                                    stream
+                                        .on('close', (code, signal) => {
+                                            console.log(
+                                                `\x1b[38;5;87mInstallation complete.\n\x1b[0m`
+                                            );
+                                            console.log(
+                                                `\x1b[38;5;83mDeployment to \x1b[38;5;214mmydevil \x1b[38;5;83msuccessful.\n\x1b[0m`
+                                            );
+                                            conn.end(); // Close the connection
+                                        })
+                                        .on('data', (data) => {
+                                            if (env.VERBOSE_LOG) {
+                                                const literal = `${data}`;
+                                                const parsed = literal.replace(
+                                                    '\n',
+                                                    ''
+                                                );
+                                                if (
+                                                    parsed.includes('audited')
+                                                ) {
+                                                    console.log(
+                                                        `\x1b[38;5;87m${parsed}\n\x1b[0m`
+                                                    );
+                                                }
+                                            }
+                                        })
+                                        .stderr.on('data', (data) => {
+                                            console.log(
+                                                `\x1b[38;5;87m${data}\n\x1b[0m`
+                                            );
+                                        });
+                                }
+                            );
+                        }
+                    );
+                });
+            })
             .on('data', (data) => {
                 console.log('STDOUT: ' + data);
             })
             .stderr.on('data', (data) => {
                 console.log('STDERR: ' + data);
-            });
-    });
-    // 2. Upload build
-    console.log('\x1b[38;5;87mCopying files.\n\x1b[0m');
-    conn.sftp((err, sftp) => {
-        if (err) throw err;
-
-        const uploadDir = (localDir, remoteDir) => {
-            fs.readdir(localDir, (err, files) => {
-                if (err) throw err;
-
-                files.forEach((file) => {
-                    const localFilePath = path.join(localDir, file);
-                    const remoteFilePath = path
-                        .join(remoteDir, file)
-                        .replace(/\\/g, '/');
-
-                    fs.stat(localFilePath, (err, stats) => {
-                        if (err) throw err;
-
-                        if (stats.isDirectory()) {
-                            sftp.mkdir(
-                                remoteFilePath,
-                                { mode: '0755' },
-                                (err) => {
-                                    if (err && err.code !== 4) throw err; // Ignore "failure" error
-                                    uploadDir(localFilePath, remoteFilePath);
-                                }
-                            );
-                        } else {
-                            sftp.fastPut(
-                                localFilePath,
-                                remoteFilePath,
-                                (err) => {
-                                    if (err) throw err;
-                                    if (env.VERBOSE_LOG) {
-                                        console.log(
-                                            `\x1b[38;5;83mUploaded: ${localFilePath} to ${remoteFilePath}\x1b[0m`
-                                        );
-                                    }
-                                }
-                            );
-                        }
-                    });
-                });
-            });
-        };
-
-        uploadDir(localDistPath, remotePath);
-    });
-
-    // 3. Run npm install in the remote build directory
-    console.log('\x1b[38;5;87mInstalling dependencies...\n\x1b[0m');
-    conn.exec(`cd ${remotePath} && npm install`, (err, stream) => {
-        if (err) throw err;
-        stream
-            .on('close', (code, signal) => {
-                console.log(`\x1b[38;5;87mnpm install complete.\n\x1b[0m`);
-                console.log(
-                    `\x1b[38;5;83mDeployment to \x1b[38;5;214mmydevil \x1b[38;5;83msuccessful.\n\x1b[0m`
-                );
-                conn.end(); // Close the connection
-            })
-            .on('data', (data) => {
-                if (env.VERBOSE_LOG) {
-                    console.log(`\x1b[38;5;87m${data}\n\x1b[0m`);
-                }
-            })
-            .stderr.on('data', (data) => {
-                console.log(`\x1b[38;5;87m${data}\n\x1b[0m`);
             });
     });
 })
@@ -120,3 +108,59 @@ conn.on('ready', () => {
         port: 22,
         tryKeyboard: true,
     });
+
+const uploadDirPromise = (sftp, localDir, remoteDir) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(localDir, (err, files) => {
+            if (err) return reject(err);
+
+            const uploadPromises = files.map((file) => {
+                const localFilePath = path.join(localDir, file);
+                const remoteFilePath = path
+                    .join(remoteDir, file)
+                    .replace(/\\/g, '/');
+
+                return new Promise((fileResolve, fileReject) => {
+                    fs.stat(localFilePath, (err, stats) => {
+                        if (err) return fileReject(err);
+
+                        if (stats.isDirectory()) {
+                            sftp.mkdir(
+                                remoteFilePath,
+                                { mode: '0755' },
+                                (err) => {
+                                    if (err && err.code !== 4)
+                                        return fileReject(err); // Ignore "failure" error
+                                    uploadDirPromise(
+                                        sftp,
+                                        localFilePath,
+                                        remoteFilePath
+                                    )
+                                        .then(fileResolve)
+                                        .catch(fileReject);
+                                }
+                            );
+                        } else {
+                            sftp.fastPut(
+                                localFilePath,
+                                remoteFilePath,
+                                (err) => {
+                                    if (err) return fileReject(err);
+
+                                    if (env.VERBOSE_LOG) {
+                                        console.log(
+                                            `\x1b[38;5;87mUploaded: ${localFilePath} to ${remoteFilePath}\x1b[0m`
+                                        );
+                                    }
+                                    fileResolve();
+                                }
+                            );
+                        }
+                    });
+                });
+            });
+
+            Promise.all(uploadPromises).then(resolve).catch(reject);
+        });
+    });
+};
