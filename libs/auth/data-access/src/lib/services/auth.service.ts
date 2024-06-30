270e5bb96d9523594246231a262e385006a7aba2
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2024-06-30 17:09:53 +0200

    AB#98 added login form

--- libs/auth/data-access/src/lib/services/auth.service.ts
+++ libs/auth/data-access/src/lib/services/auth.service.ts
@@ -12,18 +12,30 @@ export class AuthService {
     loginEndpoint = 'https://api.realworld.io/api/users/login';
     registerEndpoint = 'https://api.realworld.io/api/users';
 
-    user = signal(undefined);
+    userSignal = signal<IUser | undefined | null>(undefined);
 
     constructor(private http: HttpClient) {}
 
     login(userData: IUserLogin): void {
-        this.http.post(this.loginEndpoint, { ...userData });
+        this.http
+            .post<{ user: IUser }>(this.loginEndpoint, { user: userData })
+            .subscribe((res) => {
+                console.log('response: ', res);
+                // TODO validate
+                localStorage.setItem('token', res.user.token);
+                this.userSignal.set(res.user);
+            });
     }
 
     register(userData: IUserRegister): void {
         console.log('register from service: ', userData);
         this.http
-            .post<IUser>(this.registerEndpoint, { user: userData })
-            .subscribe((res) => console.log(res));
+            .post<{ user: IUser }>(this.registerEndpoint, { user: userData })
+            .subscribe((res) => {
+                console.log('response: ', res);
+                // TODO validate
+                localStorage.setItem('token', res.user.token);
+                this.userSignal.set(res.user);
+            });
     }
 }
--- libs/auth/feature/login/src/lib/login.component.ts
+++ libs/auth/feature/login/src/lib/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '@mobula/auth/data-access';
 
 @Component({
     selector: 'login',
@@ -16,7 +17,19 @@ export class LoginComponent {
     passwordError: string | null = null;
 
     loginForm = this.fb.nonNullable.group({
-        email: ['', Validators.required],
-        password: ['', [Validators.required, Validators.email]],
+        email: ['', Validators.required, Validators.email],
+        password: ['', [Validators.required]],
     });
+
+    constructor(private authService: AuthService) {}
+
+    submitLogin(e: Event) {
+        e.preventDefault();
+        const formData = this.loginForm.getRawValue();
+        console.log('Form data: ', formData);
+        this.authService.login({
+            email: formData.email,
+            password: formData.password,
+        });
+    }
 }
