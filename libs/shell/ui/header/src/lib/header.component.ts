30237d12a5504bce14c31aaf5a3c817f4f874381
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2024-07-03 00:38:24 +0200

    dynamic headr styles added AB#92

--- libs/shell/ui/header/src/lib/header.component.ts
+++ libs/shell/ui/header/src/lib/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { AuthService } from '@mobula/auth/data-access';
 
 @Component({
@@ -6,6 +7,19 @@ import { AuthService } from '@mobula/auth/data-access';
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
     authService = inject(AuthService);
+    router = inject(Router);
+
+    ngOnInit(): void {
+        this.router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                if (this.router.url == "/") {
+                    document.body.style.setProperty('--header-height', `65px`);
+                } else {
+                    document.body.style.setProperty('--header-height', `50px`);
+                }
+            }
+        });
+    }
 }
--- libs/shell/ui/layout/src/lib/layout.component.ts
+++ libs/shell/ui/layout/src/lib/layout.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'mobula-layout',
-  templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss',
+    selector: 'mobula-layout',
+    templateUrl: './layout.component.html',
+    styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+    router = inject(Router);
+}
